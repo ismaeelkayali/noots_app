a505f1cb83dfd4f4fa9ed0a915ad05243adfc8f4
ismaeelkayali <ismaeelkayali0@gmail.com>
2025-02-12 23:01:18 +0300

    create listview_item

---
+++ lib/views/widget/notes_listview.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:locahhost/views/widget/coustem_notes_item.dart';
+
+class NotesListview extends StatelessWidget {
+  const NotesListview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context , index){
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: CoustemNotesItem(),
+      );
+    });
+  }
+}
--- lib/views/widget/nots_app_body.dart
+++ lib/views/widget/nots_app_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'coustem_notes_item.dart';
 import 'coustom_appbar.dart';
+import 'notes_listview.dart';
 
 class NotsAppBody extends StatelessWidget {
   const NotsAppBody({super.key});
@@ -8,10 +10,12 @@ class NotsAppBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 24,left: 16,right: 16,bottom: 16),
+      padding: const EdgeInsets.only(top: 24, left: 16, right: 16, bottom: 16),
       child: Column(
         children: [
-          CoustomAppbar()
+          CoustomAppbar(),
+          SizedBox( height: 20,),
+          Expanded(child: NotesListview()),
         ],
       ),
     );
