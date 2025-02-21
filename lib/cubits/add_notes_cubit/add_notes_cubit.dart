b73c083b67580f8d2e874c05a22312080bf7d6a6
ismaeelkayali <ismaeelkayali0@gmail.com>
2025-02-21 15:34:54 +0300

    add logic add notes

--- lib/cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:locahhost/models/notes_model.dart';
 import 'package:meta/meta.dart';
 
 part 'add_notes_state.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
+
+
+
+  addNotes(NotesModel note) async{
+    emit(AddNotesLoading());
+    try {
+  var notesbox = Hive.box<NotesModel>("notes");
+  await notesbox.add(note);
+  emit(AddNotesSuccess());
+}  catch (e) {
+  emit(AddNotesfailure(e.toString()));
+}
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:locahhost/models/notes_model.dart';
 
 import 'views/nots_app_view.dart';
 
 void main() async{
   await Hive.initFlutter();
   await Hive.openBox("notes");
+  Hive.registerAdapter(NotesModelAdapter());
   runApp(const NotsApp());
 }
 
